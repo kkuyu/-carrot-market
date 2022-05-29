2d7fcba0979ace43d89c031b5f6b2e800298999a
kkuyu <devils67@naver.com>
2022-05-29 12:15:17 +0900

    Add Stream

--- pages/stream/create.tsx
+++ pages/stream/create.tsx
@@ -1,22 +1,9 @@
 import type { NextPage } from "next";
 
-const Upload: NextPage = () => {
+const Create: NextPage = () => {
   return (
     <div className="container">
       <form className="space-y-5">
-        <div>
-          <label className="w-full flex items-center justify-center h-48 text-gray-600 border-2 border-dashed border-gray-300 rounded-md hover:text-orange-500 hover:border-orange-500">
-            <svg className="h-12 w-12" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <input className="a11y-hidden" type="file" />
-          </label>
-        </div>
         <div>
           <label htmlFor="name" className="block text-sm font-semibold text-gray-700">
             Name
@@ -65,7 +52,7 @@ const Upload: NextPage = () => {
             className="w-full px-4 py-2 text-sm font-semibold text-white bg-orange-500 border border-transparent rounded-md shadow-sm
         hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
           >
-            Upload product
+            Go live
           </button>
         </div>
       </form>
@@ -73,4 +60,4 @@ const Upload: NextPage = () => {
   );
 };
 
-export default Upload;
+export default Create;
