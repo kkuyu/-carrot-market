74b366dc4c91d502eb1ab67a019e890f711cfdcf
kkuyu <devils67@naver.com>
2022-06-09 12:13:12 +0900

    Favorite Products

--- README.md
+++ README.md
@@ -126,7 +126,7 @@ Serverless Carrot Market Clone using NextJS, Tailwind, Prisma, PlanetScale and C
 - [x] 11.3 See Products
 - [x] 11.4 Product Detail
 - [x] 11.5 Related Products
-- [ ] 11.6 Favorite Products
+- [x] 11.6 Favorite Products
 - [ ] 11.7 Favorite Products part Two
 - [ ] 11.8 Bound Mutations
 - [ ] 11.9 Unbound Mutations
---
+++ pages/api/products/[id]/favorite.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import client from "@libs/client/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withSessionRoute } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const { id } = req.query;
+  const { user } = req.session;
+  const cleanId = +id?.toString()!;
+  const exists = await client.favorite.findFirst({
+    where: {
+      userId: user?.id,
+      productId: cleanId,
+    },
+  });
+  if (exists) {
+    await client.favorite.delete({
+      where: {
+        id: exists.id,
+      },
+    });
+  }
+  if (!exists) {
+    await client.favorite.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        product: {
+          connect: {
+            id: cleanId,
+          },
+        },
+      },
+    });
+  }
+  return res.status(200).json({
+    success: true,
+  });
+}
+
+export default withSessionRoute(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
